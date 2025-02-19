7cca6724f446acf7e0b70aad1d6af0c7ad02dbe7
brunokudaka <brunokudaka91@gmail.com>
2025-02-18 22:22:18 -0300

    Obtendo um usuário por id

--- src/main/java/com/brunokudaka/workshopmongo/services/UserService.java
+++ src/main/java/com/brunokudaka/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.brunokudaka.workshopmongo.services;
 
 import com.brunokudaka.workshopmongo.domain.User;
 import com.brunokudaka.workshopmongo.repository.UserRepository;
+import com.brunokudaka.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id){
+        return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
     }
 
 }
