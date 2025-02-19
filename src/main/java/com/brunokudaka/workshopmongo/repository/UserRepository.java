fc81e9ae9326758d7594567b24fc36f4378c257b
brunokudaka <brunokudaka91@gmail.com>
2025-02-18 21:16:34 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/brunokudaka/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.brunokudaka.workshopmongo.repository;
+
+import com.brunokudaka.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
